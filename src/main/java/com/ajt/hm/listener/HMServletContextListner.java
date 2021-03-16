459d77cb5ddb853951c15937dc56f07f4d272975
Ajeet <Ajeet@LAPTOP-93MNBBNC>
2021-03-17 01:53:12 +0530

    Jax-RS jars

--- src/main/java/com/ajt/hm/listener/HMServletContextListner.java
+++ src/main/java/com/ajt/hm/listener/HMServletContextListner.java
@@ -16,12 +16,15 @@ public class HMServletContextListner implements ServletContextListener {
 		System.out.println("****** Application is getting live *******");
 		DBConfig config = JsonParser.toType(DBConfig.class, new File("src/main/resources/dbconfig.json"));
 		ConnectionFactory.factory().buildConnection(config);
-		
 	}
-	
-	
+
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
+		try {
+			ConnectionFactory.factory().dispose();
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
 	}
-	
+
 }
